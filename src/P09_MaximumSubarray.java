d1979bd52dda63f4a0ab41a50ec4d6e6242c9dcf
Ankitsgh1234 <ankit2005sgh@gmail.com>
2025-10-31 22:17:12 +0530

    Maximum Subarray

---
+++ src/P09_MaximumSubarray.java
@@ -0,0 +1,21 @@
+public class P09_MaximumSubarray {
+      public static int maxSubArray(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            max=Math.max(sum,max);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+   
+   }
+     public static void main(String[] args) {
+        int[] arr={2,10,-7,5,4,1,8,6};
+       System.out.println( maxSubArray(arr));
+    }
+
+}
