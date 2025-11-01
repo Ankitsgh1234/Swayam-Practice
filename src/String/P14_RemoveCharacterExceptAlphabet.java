b7086af1c8969b1589b1b291075900ab32b67c64
Ankitsgh1234 <ankit2005sgh@gmail.com>
2025-11-01 20:54:29 +0530

    Factorial of a number

---
+++ src/String/P14_RemoveCharacterExceptAlphabet.java
@@ -0,0 +1,15 @@
+package String;
+
+public class P14_RemoveCharacterExceptAlphabet {
+    public static void remove (String sb){
+        
+        for(char ch:sb.toCharArray()){
+
+                        
+          
+
+        }
+       
+    }
+
+}
