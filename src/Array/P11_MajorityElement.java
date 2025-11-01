011b659594e7b46cd456c8da375a5d20d17879bb
Ankitsgh1234 <ankit2005sgh@gmail.com>
2025-11-01 07:51:31 +0530

    Changes in the folder

---
+++ src/Array/P11_MajorityElement.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class P11_MajorityElement {
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length / 2;
+        return nums[len];
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2,2,1,1,1,2,2 };
+        System.out.println(majorityElement(arr));
+    }
+
+}
