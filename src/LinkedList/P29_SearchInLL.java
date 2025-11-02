eb20276836a1f27277a1c95a78f139f635cf5363
Ankitsgh1234 <ankit2005sgh@gmail.com>
2025-11-02 15:53:05 +0530

    Search and Count

---
+++ src/LinkedList/P29_SearchInLL.java
@@ -0,0 +1,5 @@
+package LinkedList;
+
+public class P29_SearchInLL {
+
+}
---
+++ src/LinkedList/P30_GetCount.java
@@ -0,0 +1,24 @@
+// package LinkedList;
+
+// public class P30_GetCount {
+//     /*
+// class Node{
+//     int data;
+//     Node next;
+//     Node(int a){  data = a; next = null; }
+// }*/
+
+// class Solution {
+//     public int getCount(Node head) {
+//         // code here
+//         int i=0;
+//         Node temp=head;
+//         while(temp!=null){
+//             i++;
+//             temp=temp.next;
+//         }
+//         return i;
+//     }
+// }
+
+// }
