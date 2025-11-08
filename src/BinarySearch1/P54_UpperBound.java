3393da82506dff452a50eaada327559619fd18f9
Ankitsgh1234 <ankit2005sgh@gmail.com>
2025-11-09 00:33:04 +0530

    Upper bound

---
+++ src/BinarySearch1/P54_UpperBound.java
@@ -0,0 +1,29 @@
+package BinarySearch1;
+
+public class P54_UpperBound {
+
+     public static int Upperbound(int[] arr , int target){
+        int res=arr.length;
+        int high=arr.length-1;
+        int low=0;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>target){
+                res=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return res;
+    }
+     public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4, 6, 7};
+       
+
+        System.out.println("Lower bound: " + Upperbound(nums1,5));
+    }
+
+}
