e932b10d41fe24f0e4230cd92461c30eb80ac85b
Ankitsgh1234 <176751410+Ankitsgh1234@users.noreply.github.com>
2025-12-08 21:04:22 +0530

    Greedy

---
+++ src/Greedy2/CoinChange.java
@@ -0,0 +1,32 @@
+package Greedy2;
+
+import java.util.Arrays;
+
+public class CoinChange {
+     public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+
+        Solution sol = new Solution();
+        System.out.println(sol.coinChange(coins, amount));  
+        // Expected output: 3
+    }
+}
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+}
